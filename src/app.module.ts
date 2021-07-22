0673b7d18a1a718648a5ac5d59204d5edcf706c8
JosephOlabode <josepholabode17@gmail.com>
2021-07-22 01:57:13 +0000

    added the optional parameter to type orm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule, TypeOrmModule.forRoot()],
+  imports: [TasksModule, TypeOrmModule.forRoot(typeOrmConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
