dba560f391350ddb3d084da834156b8cdcda056d
JosephOlabode <josepholabode17@gmail.com>
2021-07-25 02:58:10 +0000

    implemented the signup dto and signup service methods

--- src/auth/user.respository.ts
+++ src/auth/user.respository.ts
@@ -4,7 +4,7 @@ import { User } from './user.entity';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
-  async signUp(authCredentialsDto: AuthCredentialsDto) {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
     const user = new User();
